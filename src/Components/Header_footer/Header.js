ac6e11a9921423bdf8aad04474b9559e2bf8e82d
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-10 22:38:47 +0300

    finished the header and created icons.js

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
 
 import { Link } from 'react-router-dom'
-
+import { CityLogo } from '../ui/icons'
 
 class Header extends Component {
     render(){
@@ -22,7 +22,13 @@ class Header extends Component {
                 <Toolbar style={{display:'flex'}}>
                     <div style={{flexGrow: 1}}>
                         <div className="header_logo">
-                            LOGO
+                            <CityLogo
+                                link={true}
+                                linkTo="/"
+                                width="70px"
+                                height="70px"
+
+                            />
                         </div>
                     </div>
 
