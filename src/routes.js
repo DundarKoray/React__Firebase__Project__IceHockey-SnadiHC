849a35dcf556d98e0afd71a661f6416a98d3f369
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-11 10:07:35 +0300

    created home, featured components for animations

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Layout from './Hoc/Layout'
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './Components/home'
 
 const Routes = (props) => {
   return (
     <div>
       <Layout>
-        the second line
+        <Switch>
+          <Route exact component={Home} path="/"/>
+        </Switch>
       </Layout>
     </div>
   )
