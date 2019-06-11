3e1987c2f6b2680624a9e5acac3f801acf993a44
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-11 12:01:44 +0300

    added animation for stripes

--- src/Components/home/featured/Stripes.js
+++ src/Components/home/featured/Stripes.js
@@ -7,21 +7,60 @@ class Stripes extends Component {
     state = {
         stripes:[
             {
-                background: '#98c5e9'
+                background: '#98c5e9',
+                left: 120,
+                rotate: 25,
+                top: -260,
+                delay: 0
             },
             {
-                background: '#ffffff'
+                background: '#ffffff',
+                left: 360,
+                rotate: 25,
+                top: -397,
+                delay: 200
             },
             {
-                background: '#98c5e9'
+                background: '#98c5e9',
+                left: 600,
+                rotate: 25,
+                top: -498,
+                delay: 400
             }
         ]
     }
 
 
-    showStripes = () => (
-        this.state.stripes.map((stripe, i)=>())
-    )
+    showStripes = () => {
+        this.state.stripes.map((stripe, i)=>(
+            <Animate 
+                key={i} 
+                show={true} 
+                
+                start={{
+                    background:'#ffffff',
+                    opacity: 0,
+                    left: 0
+                }} 
+                
+                enter={{
+                    background: [stripe.background], 
+                    timing:{delay:500, duration: 200, ease: easePolyOut},
+                    opacity: 0,
+                    left: 0
+                }}
+            >
+
+               {({ opacity, left, background})=>{
+                   return(
+                    <div className="stripe" style={{ background, opacity, transform: `rotate(0deg) translate(${left}px, 0px)` }}>
+
+                    </div>    
+                   )
+               }}
+            </Animate>
+        ))
+    }
 
     render() {
         return (
