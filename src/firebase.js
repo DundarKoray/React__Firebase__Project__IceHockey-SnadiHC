bd63325ffaafe47f7bd60d210b6c6562f118319b
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-13 14:13:35 +0300

    created firebase.js file and connected to firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/app'
+import 'firebase/database'
+
+const config = {
+    apiKey: "AIzaSyCesahK4E7bETGIkqe_1kXqBJXBMPQN6fE",
+    authDomain: "man-city-8faac.firebaseapp.com",
+    databaseURL: "https://man-city-8faac.firebaseio.com",
+    projectId: "man-city-8faac",
+    storageBucket: "man-city-8faac.appspot.com",
+    messagingSenderId: "1044080517204",
+    appId: "1:1044080517204:web:158d3d9604301c9a"
+  };
+
+  firebase.initializeApp(config);
+
+  const firebaseDB = firebase.database()
+  firebaseDB.ref('matches').once('value').then((snapshot)=>{
+      console.log(snapshot.val())
+  })
