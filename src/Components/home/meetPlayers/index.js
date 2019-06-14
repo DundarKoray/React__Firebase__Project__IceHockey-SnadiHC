09f8604700cf758f9d0078b562524a08ac1edd51
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-14 09:59:18 +0300

    added React reveal to meetPlayers component

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
 import Stripes from '../../../Resources/images/stripes.png'
 import { Tag } from '../../ui/misc'
+import Reveal from 'react-reveal/Reveal'
 
 class MeetPlayers extends Component {
 
     state = {
-
+        show: false
     }
 
     render() {
         return (
-            <div className="home_meetplayers" style={{background: `#ffffff url(${Stripes})`}}>
-                <div className="container">
-                    <div className="home_meetplayers_wrapper">
-                        <div className="home_card_wrapper">
-                            card
-                        </div>
-                        <div className="home_text_wrapper">
-                            <div>
-                                <Tag bck="#0e1731" size="100px" color="#ffffff" add={{display: 'inline-block', marginBottom: '20px'}}>Meet</Tag>
-                                <Tag bck="#0e1731" size="100px" color="#ffffff" add={{display: 'inline-block', marginBottom: '20px'}}>The</Tag>
-                                <Tag bck="#0e1731" size="100px" color="#ffffff" add={{display: 'inline-block', marginBottom: '20px'}}>Players</Tag>
+            <Reveal
+                fraction={0.7} 
+                onReveal={()=>{
+                    this.setState({
+                        show: true
+                    })
+                }} 
+            > 
+                <div className="home_meetplayers" style={{background: `#ffffff url(${Stripes})`}}>
+                    <div className="container">
+                        <div className="home_meetplayers_wrapper">
+                            <div className="home_card_wrapper">
+                                card
                             </div>
-                            <div>
-                                <Tag bck="#ffffff" size="27px" color="#0e1731" link={true} linkto="/the_team" 
-                                    add={{display: 'inline-block', marginBottom: '27px', border: '1px solid #0e1731'}}
-                                >
-                                   Meet them here 
-                                </Tag>
+                            <div className="home_text_wrapper">
+                                <div>
+                                    <Tag bck="#0e1731" size="100px" color="#ffffff" add={{display: 'inline-block', marginBottom: '20px'}}>Meet</Tag>
+                                    <Tag bck="#0e1731" size="100px" color="#ffffff" add={{display: 'inline-block', marginBottom: '20px'}}>The</Tag>
+                                    <Tag bck="#0e1731" size="100px" color="#ffffff" add={{display: 'inline-block', marginBottom: '20px'}}>Players</Tag>
+                                </div>
+                                <div>
+                                    <Tag bck="#ffffff" size="27px" color="#0e1731" link={true} linkto="/the_team" 
+                                        add={{display: 'inline-block', marginBottom: '27px', border: '1px solid #0e1731'}}
+                                    >
+                                    Meet them here 
+                                    </Tag>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Reveal>
         );
     }
 }
