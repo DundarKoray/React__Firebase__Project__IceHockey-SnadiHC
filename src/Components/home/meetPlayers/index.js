f1e6d1635d1518e6983acb1c5ba62353664c0a68
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-14 09:46:42 +0300

    added 4 tags, meet , the, players, meet them here

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -11,7 +11,27 @@ class MeetPlayers extends Component {
     render() {
         return (
             <div className="home_meetplayers" style={{background: `#ffffff url(${Stripes})`}}>
-                test
+                <div className="container">
+                    <div className="home_meetplayers_wrapper">
+                        <div className="home_card_wrapper">
+                            card
+                        </div>
+                        <div className="home_text_wrapper">
+                            <div>
+                                <Tag bck="#0e1731" size="100px" color="#ffffff" add={{display: 'inline-block', marginBottom: '20px'}}>Meet</Tag>
+                                <Tag bck="#0e1731" size="100px" color="#ffffff" add={{display: 'inline-block', marginBottom: '20px'}}>The</Tag>
+                                <Tag bck="#0e1731" size="100px" color="#ffffff" add={{display: 'inline-block', marginBottom: '20px'}}>Players</Tag>
+                            </div>
+                            <div>
+                                <Tag bck="#ffffff" size="27px" color="#0e1731" link={true} linkto="/the_team" 
+                                    add={{display: 'inline-block', marginBottom: '27px', border: '1px solid #0e1731'}}
+                                >
+                                   Meet them here 
+                                </Tag>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
