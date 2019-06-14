98b036ed640c00efcd4c50aa08770989a389a563
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-14 09:29:11 +0300

    created meetPlayers component

---
+++ src/Components/home/meetPlayers/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Stripes from '../../../Resources/images/stripes.png'
+import { Tag } from '../../ui/misc'
+
+class MeetPlayers extends Component {
+
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div className="home_meetplayers" style={{background: `#ffffff url(${Stripes})`}}>
+                test
+            </div>
+        );
+    }
+}
+
+export default MeetPlayers;
