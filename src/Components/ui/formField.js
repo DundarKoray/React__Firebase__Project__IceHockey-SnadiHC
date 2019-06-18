8b8732095ba0a7f6b38dce1ec890f30da4136717
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-18 12:44:06 +0300

    adding select, option for formfield

--- src/Components/ui/formField.js
+++ src/Components/ui/formField.js
@@ -36,6 +36,29 @@ const FormField = ({formdata, id, change}) => {
                     </div>
                 )
             break;
+            case('select'):
+                formTemplate = (
+                    <div>
+                        {formdata.showlabel ?
+                            <div className="label_inputs">
+                                {formdata.config.label}
+                            </div>
+                            :null
+                        }
+                        <select value={formdata.value} onChange={(event)=> change({event, id})}>
+                            <option value="">Select one</option>
+                            {
+                                formdata.config.options.map((item)=>(
+                                    <option key={item.key} value={item.key}>
+                                        {item.value}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                        { showError() }
+                    </div>
+                )
+            break;
             default:
                 formTemplate = null
         }
