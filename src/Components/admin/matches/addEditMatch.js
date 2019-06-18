d72fb7eaa05f65a46b17fee47d9d9d82b6ad7b95
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-18 12:32:52 +0300

    created form for add games

--- src/Components/admin/matches/addEditMatch.js
+++ src/Components/admin/matches/addEditMatch.js
@@ -12,7 +12,37 @@ class AddEditMatch extends Component {
         formSuccess: '',
         teams: [],
         formdata:{
-            
+            date:{
+                element: 'input',
+                value: '',
+                config:{
+                    label: 'Event date',
+                    name: 'date_input',
+                    type: 'date',
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                validationMessage:'',
+                showlabel: true
+            },
+            local:{
+                element: 'select',
+                value: '',
+                config:{
+                    label: 'Select a local team',
+                    name: 'select_local',
+                    type: 'select',
+                    options: []
+                },
+                validation:{
+                    required: true
+                },
+                valid: false,
+                validationMessage:'',
+                showlabel: false
+            },
         }
     }
 
@@ -20,7 +50,15 @@ class AddEditMatch extends Component {
         return (
             <div>
                 <AdminLayout>
-                    add edit match
+                    <div className="editmatch_dialog_wrapper">
+                        <h2>{this.state.formType}</h2>
+                        <div>
+                            <form onSubmit={(event)=> this.submitForm(event)}>
+                                <FormField id={'date'} formdata={this.state.formdata.date} change={(element)=> this.updateForm(element)}/>
+                                <FormField id={'local'} formdata={this.state.formdata.local} change={(element)=> this.updateForm(element)}/>
+                            </form>
+                        </div>
+                    </div>
                 </AdminLayout>
             </div>
         );
