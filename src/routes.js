90388a6f370349e066842c9dbb44ab70f5b4162e
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-18 10:38:44 +0300

    added private route for admin_matches

--- src/routes.js
+++ src/routes.js
@@ -9,13 +9,15 @@ import Home from './Components/home'
 import SignIn from './Components/signin'
 
 import Dashboard from './Components/admin/dashboard'
+import AdminMatches from './Components/admin/matches'
 
 const Routes = (props) => {
   return (
     <div>
       <Layout>
         <Switch>
-
+          
+          <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches}/>
           <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
           <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn}/>
           <PublicRoute {...props} restricted={false} path="/" exact component={Home}/>
