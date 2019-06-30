f02c40b4f0577829b6b3099c7a86eed2dad5ee7b
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-30 15:56:15 +0300

    created AddEditPlayers

---
+++ src/Components/admin/players/addEditPlayers.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import AdminLayout from '../../../Hoc/AdminLayout'
+
+import FormField from '../../ui/formField'
+import { validate } from '../../ui/misc'
+
+import { firebaseTeams, firebaseDB, firebaseMatches } from '../../../firebase'
+import { firebaseLooper } from '../../ui/misc'
+
+
+class AddEditPlayers extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AddEditPlayers
