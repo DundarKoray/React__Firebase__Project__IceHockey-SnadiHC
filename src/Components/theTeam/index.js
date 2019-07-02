2f7c6403819a678624b581fbe44c42b46dded73a
Koray Dundar <koraydundar_40@hotmail.com>
2019-07-02 13:41:34 +0300

    getting url for all the player images using promis

--- src/Components/theTeam/index.js
+++ src/Components/theTeam/index.js
@@ -8,7 +8,44 @@ import { firebaseLooper } from '../ui/misc'
 import { Promise } from 'core-js'
 
 class TheTeam extends Component {
+
+    state = {
+        loading: true,
+        players: []
+    }
+
+    componentDidMount(){
+        firebasePlayers.once('value').then(snapshot =>{
+            const players = firebaseLooper(snapshot)
+            console.log(players)
+
+            //getting url for all images
+            let promises = []
+            for (let key in players){
+                promises.push(
+                    new Promise((resolve, reject)=>{
+                        firebase.storage().ref('players')
+                        .child(players[key].image).getDownloadURL()
+                        .then( url => {
+                            players[key].url = url
+                            resolve()
+                        })
+                    })
+                )
+            }
+
+            Promise.all(promises).then(()=>{
+                this.setState({
+                    loading: false,
+                    players
+                })
+            })
+            
+        })
+    }
+
     render() {
+        console.log(this.state.players)
         return (
             <div>
                 the team
