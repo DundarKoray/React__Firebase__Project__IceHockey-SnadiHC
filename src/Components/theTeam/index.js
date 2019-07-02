664539516422e41eca2b4dd1eff38d8be4f92bdb
Koray Dundar <koraydundar_40@hotmail.com>
2019-07-02 13:22:57 +0300

    created TheTeam component and added in routes

---
+++ src/Components/theTeam/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TheTeam extends Component {
+    render() {
+        return (
+            <div>
+                the team
+            </div>
+        );
+    }
+}
+
+export default TheTeam
