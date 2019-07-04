5534c6868965181785c75be7ec9950b300d4808f
Koray Dundar <koraydundar_40@hotmail.com>
2019-07-04 15:43:38 +0300

    updated 404 pagenot found and deployed the project

--- src/Components/ui/not_found.js
+++ src/Components/ui/not_found.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import not_found from '../../Resources/images/page_not_found.png'
 
 const Not_found = () => {
     return (
         <div className="not_found_container">
-            <div>Sorry :(</div>
-            <div>Page not found</div>
+            <div>404 - Page not found :(</div>
+            <div>Where is the page Aguero?</div>
+            <img style={{width: '30%', opacity: '0.75'}} src={not_found}/>
         </div>
     )
 }
